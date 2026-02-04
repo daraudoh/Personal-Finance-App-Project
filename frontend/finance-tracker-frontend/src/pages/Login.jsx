f6f7e9001ab4c92305b5f931ffece4a336866322
Dara Williford <162847659+daraudoh@users.noreply.github.com>
2026-02-03 17:33:37 -0800

    feat: implement login and registration forms with API integration

--- frontend/finance-tracker-frontend/src/pages/Login.jsx
+++ frontend/finance-tracker-frontend/src/pages/Login.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+import API from '../api';
+import { useNavigate } from 'react-router-dom';
+
 export default function Login() {
+  const [form, setForm] = useState({ email: '', password: ''});
+  const navigate = useNavigate();
+
+  const handleChange = e => setForm({...form, [e.target.name]: e.target.value});
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const res = await API.post('/auth/login', form);
+    localStorage.setItem('token', res.data.token);
+    navigate('/dashboard');
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input name="email" placeholder="Email" onChange={handleChange} />
+      <input name="password" type="password" placeholder="Password" onChange={handleChange} />
+      <button type="submit">Login</button>
+    </form>
   );
 }
--- frontend/finance-tracker-frontend/src/pages/Register.jsx
+++ frontend/finance-tracker-frontend/src/pages/Register.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import API from '../api';
+import { useNavigate } from 'react-router-dom';
+
 export default function Register() {
+  const [from, setForm] = useState({ name: '', email:'', password: ''});
+  const navigate = useNavigate();
+
+  const handleChange = e => setForm({...form, [e.target.name]: e.target.value});
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    await API.post('/auth/register', form);
+    navigate('/login');
+
+  };
+
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <form onSubmit = {handleSubmit}>
+      <input name="name" placeholder="Name" onChange={handleChange} />
+      <input name="email" placeholder="Email" onChange={handleChange} />
+      <input name="password" type="password" placeholder="Password" onChange={handleChange} />
+      <button type= "submit">Register</button>
+    </form>
   );
+  
 }
